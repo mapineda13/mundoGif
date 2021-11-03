2de22a4f38740b83c8903476dd30cb9107c7a259
Milton Pineda <mpineda@grupomerelec.com>
2021-11-03 13:44:24 -0600

    cambios en buscador de gifs

---
+++ gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent  {
+
+  constructor(private gifService:GifsService) { }
+
+
+  get historial(){
+    return this.gifService.historial;
+  }
+
+}
